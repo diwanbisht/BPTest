e28fb5a8f5d54af42015a14386bcc3c9a8b12a6e
Diwan Bisht <diwan.bisht@gmail.com>
2023-08-04 22:10:43 +0530

    Add project files.

---
+++ BPTest/NextBithdDate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BPTest
+{
+    public class NextBithdDate
+    {
+        public static DateTime GetNextNextBirthday(DateTime birthdate)
+        {
+            DateTime nextNextBirthday;
+            DateTime today = DateTime.Today;
+            DateTime nextBirthday = new DateTime(today.Year, birthdate.Month, birthdate.Day);
+
+            if (nextBirthday <= today)
+            {
+                nextNextBirthday = nextBirthday.AddYears(1);
+            }
+            else
+            {
+                nextNextBirthday = nextBirthday.AddYears(1);
+            }
+            return nextNextBirthday;
+        }
+
+        public static string ValidateDateTimeFormat(String GetNextBirthDay)
+        {
+            String[] BirthDayMothYear = GetNextBirthDay.Split('-');
+            if (BirthDayMothYear[1].Length == 1)
+            {
+                BirthDayMothYear[1] = "0" + BirthDayMothYear[1];
+            }
+            if (BirthDayMothYear[2].Length == 1)
+            {
+                BirthDayMothYear[2] = "0" + BirthDayMothYear[2];
+            }
+            /* if (BirthDay[0].Length == 2)
+             {
+                 BirthDay[0] = "00" + BirthDay[0];
+             }*/
+            return BirthDayMothYear[0] + "-" + BirthDayMothYear[1] + "-" + BirthDayMothYear[2];
+        }
+
+
+
+    }
+}
---
+++ BPTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace BPTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your Birthdate:");
+            String GetNextBirthDay = Console.ReadLine();
+
+            if (GetNextBirthDay == null)
+            {
+                Console.WriteLine("Birth Date value is  Null.." + GetNextBirthDay);
+            }
+            else if (GetNextBirthDay == String.Empty)
+            {
+                Console.WriteLine("Birth Date value is  Empty.." + GetNextBirthDay);
+            }
+            else if (GetNextBirthDay.Length < 8)
+            {
+                Console.WriteLine("Birth Date formate is not valid.." + GetNextBirthDay);
+            }
+
+            //string GetNextBirthDay = "1999-7-30"; // Replace with the birthdate in "yyyy-MM-dd" format
+
+            // Possible null reference argument.
+            else
+            {
+                GetNextBirthDay = NextBithdDate.ValidateDateTimeFormat(GetNextBirthDay);
+            }
+
+
+            if (Regex.IsMatch(GetNextBirthDay, @"^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$"))
+            {
+
+                DateTime birthdate = DateTime.ParseExact(GetNextBirthDay, "yyyy-MM-dd", null);
+                DateTime nextNextBirthday = NextBithdDate.GetNextNextBirthday(birthdate);
+
+
+                Console.WriteLine("Your Next Birthday: " + nextNextBirthday.ToString("MMMM dd, yyyy"));
+            }
+            else
+            {
+                Console.WriteLine("Invalid birthdate format.");
+            }
+
+
+        }
+    }
+}
